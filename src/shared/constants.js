ad045801d8058e2bb086bb14ea685a8511152810
BIT Student <classroom@bgit.rs>
2018-06-21 11:57:36 +0200

    fixing

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 const baseEndpoint = 'http://bitbookapi.azurewebsites.net/api';
-const postEndpoint = 'http://bitbookapi.azurewebsites.net/api/Posts?$top=30';
+const postEndpoint = 'http://bitbookapi.azurewebsites.net/api/Posts';
 const commentsEndpoint = 'http://bitbookapi.azurewebsites.net/api/Comments?postId=';
 const newCommentEndpoint = "http://bitbookapi.azurewebsites.net/api/Comments";
 const userEndpoint = 'http://bitbookapi.azurewebsites.net/api/users';
@@ -9,19 +9,34 @@ const videoPostEndpoint = 'http://bitbookapi.azurewebsites.net/api/VideoPosts';
 const profileEndpoint = 'http://bitbookapi.azurewebsites.net/api/profile';
 const userEditProfileEndpoint = 'http://bitbookapi.azurewebsites.net/api/Profiles';
 const photoUploadEndpoint = 'http://bitbookapi.azurewebsites.net/api/upload';
-
 const registerEndpoint = "http://bitbookapi.azurewebsites.net/api/register";
 const loginEndpoint = "http://bitbookapi.azurewebsites.net/api/login";
 
-const requestsHeader = {
-    'Content-Type': 'application/json',
-    'Key': 'bitbookdev',
-    'SessionId': '2990B489-DB94-4AC1-ACDE-CDC9CC3EAEAE'
-}
+// const requestsHeader = {
+//     'Content-Type': 'application/json',
+//     'Key': 'bitbookdev',
+//     'SessionId': '2990B489-DB94-4AC1-ACDE-CDC9CC3EAEAE'
+// }
 
 const registerPostHeaders = {
     'Content-Type': 'application/json',
     'Key': 'bitbookdev',
 }
 
-export { baseEndpoint, postEndpoint, requestsHeader, commentsEndpoint, userEndpoint, newCommentEndpoint, textPostEndpoint, imagePostEndpoint, videoPostEndpoint, profileEndpoint, userEditProfileEndpoint, registerEndpoint, registerPostHeaders, loginEndpoint, photoUploadEndpoint }
+export { 
+    baseEndpoint, 
+    postEndpoint, 
+    // requestsHeader, 
+    commentsEndpoint, 
+    userEndpoint, 
+    newCommentEndpoint, 
+    textPostEndpoint, 
+    imagePostEndpoint, 
+    videoPostEndpoint, 
+    profileEndpoint, 
+    userEditProfileEndpoint, 
+    registerEndpoint, 
+    registerPostHeaders, 
+    loginEndpoint, 
+    photoUploadEndpoint 
+}
